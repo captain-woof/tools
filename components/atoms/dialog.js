d7252531b266ce612b8f5edd8474355dec20f8f7
Sohail Saha <captainwoof.official@gmail.com>
2021-10-26 01:11:05 +0530

    Dialogbox comp made

---
+++ components/atoms/dialog.js
@@ -0,0 +1,86 @@
+import styled, { css } from "styled-components"
+import { IoMdClose } from 'react-icons/io'
+import { useCallback } from "react"
+
+const Backdrop = styled.div`
+    ${({ theme }) => css`
+        /* Appear animation */
+        @keyframes appear {
+            0% {
+                opacity: 0;
+            }
+
+            100% {
+                opacity: 0.95;
+            }
+        }
+
+        position: fixed;        
+        height: 100vh;
+        width: 100vw;
+        top: 0;
+        left: 0;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-wrap: nowrap;
+        z-index: 100;
+        background-color: ${theme.colors.secondary.dark};
+        animation: appear 0.5s linear both;
+        margin: 0;
+    `}
+`
+
+const DialogBox = styled.div`
+    ${({ theme, wide }) => css`
+        position: relative;
+        z-index: 101;
+        height: ${wide ? '60vh' : 'clamp(280px, 30vh, 320px)'};
+        width: ${wide ? '60vw' : 'clamp(320px ,30vw, 360px)'};
+        background-color: ${theme.colors.background.light};
+        box-shadow: 0 16px 24px ${theme.colors.secondary.dark};
+        padding: var(--sp-400);
+        opacity: 1;
+
+        @media (max-width: 480px){
+            height: 80vh;
+            width: 80vw;
+        }
+    `}    
+`
+
+const CloseIcon = styled(IoMdClose)`
+    ${({ theme }) => (css`
+        position: absolute;
+        fill: ${theme.colors.secondary.dark};
+        top: var(--sp-400);
+        right: var(--sp-400);
+        height: 1.35rem;
+        width: 1.35rem;
+        cursor: pointer;
+        z-index: 102;
+
+        &:hover{
+            fill: ${theme.colors.secondary.light};
+        }
+    `)}    
+`
+
+export default function Dialog({ children, backdropStyle, dialogBoxStyle, wide, isOpen, setIsOpen }) {
+    const handleClose = useCallback((e) => {
+        if (e.target === e.currentTarget) {
+            setIsOpen(false)
+        }
+    }, [setIsOpen])
+
+    return (
+        isOpen &&
+        <Backdrop style={backdropStyle} className='dialog-backdrop' onClick={handleClose}>
+            <DialogBox style={dialogBoxStyle} className='dialog-box' wide={wide}>
+                <CloseIcon onClick={handleClose} />
+                {children}
+            </DialogBox>
+        </Backdrop>
+
+    )
+}
