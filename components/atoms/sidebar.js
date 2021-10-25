b4d445b8bfd7a5fb9bda64a6273bb94ec5f0d972
Sohail Saha <captainwoof.official@gmail.com>
2021-10-25 22:20:16 +0530

    Sidebar appears fixed at bottom for mobile

--- components/atoms/sidebar.js
+++ components/atoms/sidebar.js
@@ -6,6 +6,7 @@ const StyledSidebar = styled.aside`
         top: 0;
         left: 0;
         height: max-content;
+        min-width: 320px;
         width: 320px;
         border-right: 4px solid ${theme.colors.primary.main};
         background-color: ${theme.colors.background.light};
@@ -13,15 +14,22 @@ const StyledSidebar = styled.aside`
 
         @media (max-width: 768px){
             width: 235px;
+            min-width: 235px;
         }
 
         @media (max-width: 480px){
             & {
-                height: fit-content;
+                min-width: 100%;
+                position: fixed;
+                height: 30vh;
+                overflow-y: auto;
+                bottom: 0;
+                left: 0;
+                top: unset;
+                box-shadow: 0 -4px 8px ${theme.colors.secondary.light};
                 width: 100%;
-                border-right: none;
-                box-shadow: 0 1px 6px ${theme.colors.secondary.light};
-                padding: var(--sp-400);
+                border-right: none;                
+                border-radius: 36px 36px 0 0;
             }
         }
     `}
