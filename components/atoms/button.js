57c34c4fad938a6d1c57d52973ac6f23ae84a28e
Sohail Saha <captainwoof.official@gmail.com>
2021-10-26 10:05:27 +0530

    Button modified

--- components/atoms/button.js
+++ components/atoms/button.js
@@ -4,10 +4,11 @@ const StyledButton = styled.button`
     ${({ theme, primary, small }) => (css`
         display: flex;
         align-items: center;
-        flex-wrap: nowrap;
+        justify-content: center;
+        flex-wrap: wrap;
         gap: 0 0.5rem;
         padding: ${small ? 'var(--sp-200) var(--sp-400)' : 'var(--sp-300) var(--sp-500)'};
-        background-color: ${primary ? theme.colors.primary.main : theme.colors.secondary.light};
+        background-color: ${primary ? theme.colors.primary.main : theme.colors.secondary.main};
         border-radius: 4px;
         color: #fff;
         font-weight: 500;
@@ -27,11 +28,14 @@ const StyledButton = styled.button`
         & svg {
             width: ${small ? '1.2rem' : '1.4rem'};
             height: ${small ? '1.2rem' : '1.4rem'};
+            stroke: #fff;
+            color: #fff;
+            fill: #fff;
         }
     `)}
 `
 
-export default function Button({ children, primary, buttonProps, small = false, iconEnd, iconStart }) {
+export default function Button({ children, primary, buttonProps, small = false, iconEnd, iconStart, hideTextOnMobile = false }) {
     return (
         <StyledButton {...buttonProps} primary={primary} small={small}>
             {iconStart}
