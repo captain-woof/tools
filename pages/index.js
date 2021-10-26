73373bee175927160a0d1944456cdb3049e57f34
Sohail Saha <captainwoof.official@gmail.com>
2021-10-26 11:46:51 +0530

    Added SEO to homepage

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,7 @@ import { LinkExternal } from '../components/atoms/link'
 import Textfield from '../components/atoms/textfield'
 import styled from 'styled-components'
 import { useState } from 'react'
+import Seo from '../components/atoms/seo'
 
 const SearchboxWrapper = styled.div`
     display: flex;
@@ -33,22 +34,25 @@ export default function Index() {
     const [searchTerm, setSearchTerm] = useState('')
 
     return (
-        <LobotomizedContainer>
-            <Heading1 style={{ margin: '0 0 0 1rem', userSelect: 'none' }}>
-                Tools
-            </Heading1>
-            <p style={{ marginLeft: '1rem' }}>Below is a collection of tools I created. Almost every tool I make will be accompanied by <LinkExternal style={{ fontWeight: '600' }} href="https://sohail-saha.in/blog/">a blog post.</LinkExternal></p>
-            <p style={{ marginLeft: '1rem' }}>Feel free to use any of them for your work.</p>
-            <SearchboxWrapper>
-                <Textfield label="Search" name="search" style={{ width: 'clamp(240px, 30%, 300px)' }} inputProps={{ value: searchTerm, onChange: (e) => { setSearchTerm(e.target.value) } }} />
-            </SearchboxWrapper>
-            <Grid wrap style={{ marginTop: 'var(--sp-500)' }}>
-                {toolsData
-                .filter((toolData) => {return toolData.name.toLowerCase().indexOf(searchTerm) !== -1})
-                .map((toolData, index) => (
-                    <Toolcard key={index} name={toolData.name} description={toolData.description} url={toolData.url} />
-                ))}
-            </Grid>
-        </LobotomizedContainer>
+        <>
+            <Seo title="Tools by Sohail" description="This is the homepage of 'Tools by Sohail', a site housing tools to aid in development." url={`https://${process.env.NEXT_PUBLIC_APP_ORIGIN}/`} keywords="dev tools, developer tools, open source tools, frontend tools, frontend, sohail saha, captain woof, captain-woof"/>
+            <LobotomizedContainer>
+                <Heading1 style={{ margin: '0 0 0 1rem', userSelect: 'none' }}>
+                    Tools
+                </Heading1>
+                <p style={{ marginLeft: '1rem' }}>Below is a collection of tools I created. Almost every tool I make will be accompanied by <LinkExternal style={{ fontWeight: '600' }} href="https://sohail-saha.in/blog/">a blog post.</LinkExternal></p>
+                <p style={{ marginLeft: '1rem' }}>Feel free to use any of them for your work.</p>
+                <SearchboxWrapper>
+                    <Textfield label="Search" name="search" style={{ width: 'clamp(240px, 30%, 300px)' }} inputProps={{ value: searchTerm, onChange: (e) => { setSearchTerm(e.target.value) } }} />
+                </SearchboxWrapper>
+                <Grid wrap style={{ marginTop: 'var(--sp-500)' }}>
+                    {toolsData
+                        .filter((toolData) => { return toolData.name.toLowerCase().indexOf(searchTerm) !== -1 })
+                        .map((toolData, index) => (
+                            <Toolcard key={index} name={toolData.name} description={toolData.description} url={toolData.url} />
+                        ))}
+                </Grid>
+            </LobotomizedContainer>
+        </>
     )
 }
