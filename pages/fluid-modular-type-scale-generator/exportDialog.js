aadcc9040724e710deca97b4c5a18b1b47b51dac
Sohail Saha <captainwoof.official@gmail.com>
2021-10-26 08:47:33 +0530

    Made changes to integrate new Dialog

--- pages/fluid-modular-type-scale-generator/exportDialog.js
+++ pages/fluid-modular-type-scale-generator/exportDialog.js
@@ -1,12 +1,18 @@
 import Dialog from "../../components/atoms/dialog";
 import { Heading3 } from "../../components/atoms/headings";
+import Codeblock from "./codeBlock";
 import { useGlobalState } from "./globalState";
 
 export default function ExportDialog({ wide }) {
     const { state, dispatch } = useGlobalState()
+
     return (
-        <Dialog wide isOpen={state?.exportDialogOpen} setIsOpen={(newIsOpen) => { dispatch({ type: 'SET_EXPORT_DIALOG_OPEN', isOpen: newIsOpen }) }}>
+        <Dialog wide isOpen={state?.exportDialogOpen} setIsOpen={(newIsOpen) => { dispatch({ type: 'SET_EXPORT_DIALOG_OPEN', isOpen: newIsOpen }) }} dialogBoxStyle={{
+            display: 'flex',
+            flexDirection: 'column'
+        }}>
             <Heading3>Export</Heading3>
+            <Codeblock />
         </Dialog>
     )
 }
